741dbafb260019741f42ac3b48fa0b5cb7672765
Aathi <aathithirunav@gmail.com>
2026-06-23 23:21:07 +0530

    Add UserService class for user registration and login functionality

---
+++ src/com/studentmanagementsystem/service/UserService.java
@@ -0,0 +1,65 @@
+package src.com.studentmanagementsystem.service;
+
+import java.util.Scanner;
+
+import src.com.studentmanagementsystem.model.User;
+
+public class UserService {
+
+    LoginSystem loginSystem = new LoginSystem();
+
+    public void register(Scanner sc) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'register'");
+    }
+
+    public User login(Scanner sc) throws Exception {
+        while(true){
+            System.out.println("\n--- Select Role ---");
+            System.out.println("1. Admin Login");
+            System.out.println("2. Faculty Login");
+            System.out.println("3. Student Login");
+            System.out.print("Enter Choice: ");
+            
+            int roleChoice = Integer.parseInt(sc.nextLine());
+            String role = "";
+
+            switch (roleChoice) {
+                case 1:
+                    role = "admin";
+                    break;
+                case 2: 
+                    role = "faculty";
+                    break;
+                case 3 :
+                    role =  "student";
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+                    throw new Exception("Invalid Choice");
+            };
+
+            if (role.isEmpty()) {
+                System.out.println("Invalid role! Back to main menu.");
+                continue;
+            }
+
+            System.out.print("Enter username: ");
+            String uname = sc.nextLine();
+            System.out.print("Enter password: ");
+            String pass = sc.nextLine();
+
+            User loggedIn = loginSystem.login(uname, pass, role);
+            
+            if (loggedIn == null) {
+                System.out.println("Invalid login! Try again.");
+                continue; 
+            }
+
+            System.out.println("Login successful! Role: " + loggedIn.getRole());
+            return loggedIn;
+        }
+        
+    }
+    
+}
