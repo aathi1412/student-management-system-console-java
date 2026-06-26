86dd2c44976f2437d3672cfe9122eb31a25a98b3
Aathi <aathithirunav@gmail.com>
2026-06-26 14:46:18 +0530

    Add id field and corresponding getter/setter methods to Student class

--- src/com/studentmanagementsystem/model/Student.java
+++ src/com/studentmanagementsystem/model/Student.java
@@ -1,12 +1,16 @@
 package src.com.studentmanagementsystem.model;
 
 public class Student {
+    private int id;
     private String name;
     private int rollNo;
     private String dob;
     private int attendance;
     private boolean feesPaid;
     private String personalDetails;
+
+    public Student() {
+    }
 
     public Student(String name, int rollNo, String dob, int attendance, boolean feesPaid, String personalDetails) {
         this.name = name;
@@ -17,8 +21,61 @@ public class Student {
         this.personalDetails = personalDetails;
     }
 
-    public int getRollNo() { return rollNo; }
-    public String getName() { return name; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRollNo() { 
+        return rollNo; 
+    }
+    
+    public String getName() {
+        return name; 
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(int attendance) {
+        this.attendance = attendance;
+    }
+
+    public boolean isFeesPaid() {
+        return feesPaid;
+    }
+
+    public void setFeesPaid(boolean feesPaid) {
+        this.feesPaid = feesPaid;
+    }
+
+    public String getPersonalDetails() {
+        return personalDetails;
+    }
+
+    public void setPersonalDetails(String personalDetails) {
+        this.personalDetails = personalDetails;
+    }
 
     public void update(String name, String dob, int attendance, boolean feesPaid, String personalDetails) {
         this.name = name;
@@ -37,4 +94,8 @@ public class Student {
         System.out.println("Fees Paid: " + (feesPaid ? "Yes" : "No"));
         System.out.println("Personal Details: " + personalDetails);
     }
+
+
+
+    
 }
